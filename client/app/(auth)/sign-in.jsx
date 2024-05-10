2a4c3f1a15b48495b4916fac8374cc1308eeb384
Ryan <ryanpvoong@gmail.com>
2024-05-10 17:06:14 -0400

    added sign up page. fixed sign in page

--- client/app/(auth)/sign-in.jsx
+++ client/app/(auth)/sign-in.jsx
@@ -22,13 +22,13 @@ const SignIn = () => {
       <ScrollView>
         <View className='w-full justify-center min-h-[85vh] px-4 my-6'>
           <Image source={ images.logo } resizeMode='contain' className='w-[115px] h-[35px]' />
-          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Log in to Aora</Text>
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>Sign up to Aora</Text>
 
           <FormField 
             title="Email"
             value={ form.email }
             handleChangeText={ (e) => setForm({ ...form, 
-              email: encodeURIComponent
+              email: e
             })}
             otherStyles="mt-7"
             keyboardType="email-address"
